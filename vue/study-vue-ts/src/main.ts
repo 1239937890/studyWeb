f4ae86b234f79a806c86b2b810d65441750cab89
灵感仰 <44220593+1239937890@users.noreply.github.com>
2023-01-09 14:06:47 +0800

    合并代码

---
+++ vue/study-vue-ts/src/main.ts
@@ -0,0 +1,19 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+import './assets/main.css'
+
+// Toast
+import 'vant/es/toast/style';
+
+import 'amfe-flexible/index.js';
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
---
+++ vue/study-vue-ts/src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+  // import TheWelcome from '../components/TheWelcome.vue'
+  import {
+    showToast
+  } from 'vant';
+  showToast('提示内容');
+</script>
+
+<template>
+  <main>
+    <!-- <TheWelcome /> -->
+    <div class="demo">你好</div>
+  </main>
+</template>
+<style>
+  .demo {
+    width: 413px;
+    height: 72px;
+    border-radius: 36px;
+    background-color: red;
+  }
+</style>
